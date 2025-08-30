3ccf12ad459e3af21a66daa02edcdece355e8c3f
Eric Paschke <edpaschke@gmail.com>
2025-08-29 20:55:27 -0400

    fixed math when converting from joystick to currents

--- src/teleop/src/teleop.cpp
+++ src/teleop/src/teleop.cpp
@@ -35,13 +35,19 @@ public:
 
         float x_axis = SDL_GameControllerGetAxis(controller_, SDL_CONTROLLER_AXIS_LEFTX) / 32767.0f;
         float y_axis = SDL_GameControllerGetAxis(controller_, SDL_CONTROLLER_AXIS_LEFTY) / 32767.0f;
-        RCLCPP_INFO(this->get_logger(), "left: %f, right: %f", x_axis, y_axis);
+        RCLCPP_INFO(this->get_logger(), "x: %f, y: %f", x_axis, y_axis);
         // Get linear and angular component of robot based on max speed
         float linear_component = y_axis * FULL_FORWARD_MAGNITUDE;
         float angular_component = x_axis * (1 - FULL_FORWARD_MAGNITUDE);
         // Get wheel speeds based on linear and angular components (currently using arcade drive)
-        Uint8 left_wheels_speed = (Uint8) ((linear_component - angular_component) * 127) + 127;
-        Uint8 right_wheels_speed = (Uint8) ((linear_component + angular_component) * 127) + 127;
+        auto convertToCurrent = [](float speed) -> Uint8 {
+          float scaled_speed = ((speed + 1.0f) / 2.0f) * 254.0f;
+          if(scaled_speed < 0) scaled = 0;
+          else if(scaled_speed > 254) scaled = 254;
+          return static_cast<Uint8>(scaled_speed);
+        }
+        Uint8 left_wheels_speed = convertToCurrent(linear_component - angular_component);
+        Uint8 right_wheels_speed = convertToCurrent(linear_component + angular_component);
         // Send wheel speeds to serial node
         auto message = serial_msgs::msg::MotorCurrents();
         message.front_left = left_wheels_speed;
