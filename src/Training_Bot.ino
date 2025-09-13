438fa1f65342d02775efd22c6ee90a9cf26ebd1d
Carlos Giron <165074942+Hemo-Dyn@users.noreply.github.com>
2025-09-13 00:07:42 -0400

    Refactor drive functions and update pin mappings
    Expirimented with having more logic pins in parallel to use less digital pins on the training bot. It seemed to work following testing.

--- src/Training_Bot.ino
+++ src/Training_Bot.ino
@@ -2,29 +2,34 @@
 Carlos Giron
 */
 
+/* NOTES:
+* For using tele-op, change func in main to "control_process()" 
+* Need to clean up this code by writing a header file for functions and testing and tele-op stuff
+*/
+
 // ---------------------
 // Preprocessor
 // ---------------------
 
 #include <stdint.h>
 
-
-#define FR_EN 		  3
+#define FR_EN 		3
 #define FR_FORWARD  4
 #define FR_BACKWARD 5
 
-#define FL_EN		    6
+#define FL_EN		6
 #define FL_FORWARD 	7
-#define FL_BACKWARD 13
+#define FL_BACKWARD 8
 
-#define BL_EN		    9
-#define BL_FORWARD  10
+#define BL_EN		6
+#define BL_FORWARD  7
 #define BL_BACKWARD 8
 
-#define BR_EN		    11
-#define BR_FORWARD	2
-#define BR_BACKWARD	12
+#define BR_EN		3
+#define BR_FORWARD	4
+#define BR_BACKWARD	5
 
+#define ZERO  126
 #define SPEED 255
 
 // ---------------------
@@ -47,27 +52,32 @@ class Wheel {
   	  pinMode(pinF , OUTPUT);
       pinMode(pinB , OUTPUT);
 	}
+
+  void drive(uint8_t duty, bool isForward) {
+    digitalWrite(pinF,  isForward);
+    digitalWrite(pinB, !isForward);
+    analogWrite(pinEn, duty);
+  }
   
-  void drive(uint8_t input) {
-    bool    direction;
+  void tele_drive(uint8_t input) {
+    bool    isForward;
     uint8_t duty;
 
-    if (input < 127) {
-      duty = (127 - input) << 1;
-      direction = false;
+    if (input < ZERO) {
+      duty = (ZERO - input) << 1;
+      isForward = false;
     }
-    else if (input >= 127) {
-      duty = (input - 127) << 1;
-      direction = true;
+    else if (input >= ZERO) {
+      duty = (input - ZERO) << 1;
+      isForward = true;
     }
-    digitalWrite(pinF,  direction);
-    digitalWrite(pinB, !direction);
-    analogWrite(pinEn, duty);
-  } 
+    drive(duty, isForward);
+  }
 };
 
+
 // ---------------------
-// Setup & Loop
+// setting up wheels and funcs for tele-op control or testing 
 // ---------------------
 
 Wheel frontLeft(FL_EN, FL_FORWARD, FL_BACKWARD);
@@ -75,7 +85,34 @@ Wheel frontRight(FR_EN, FR_FORWARD, FR_BACKWARD);
 Wheel backLeft(BL_EN, BL_FORWARD, BL_BACKWARD);
 Wheel backRight(BR_EN, BR_FORWARD, BR_BACKWARD);
 
-Wheel wheelAr[4] = {frontLeft, frontRight, backLeft, backRight};
+Wheel wheelAr[4] = {frontLeft, backLeft,frontRight, backRight};
+
+void control_process(void) {
+  if (Serial.available() > 0) {
+
+    byte data[2]; // Left, Right 
+    Serial.readBytes(data, 2);
+
+    for (int i=0; i<2; i++) {
+      wheelAr[i*2].tele_drive(data[i]);
+      wheelAr[i*2+1].tele_drive(data[i]);
+    }
+  }
+}
+
+bool forward = true;
+void test_process(void) {
+  for (int i=0; i<2; i++) {
+    wheelAr[i].test_drive(150, forward);
+    wheelAr[i+1].test_drive(150, !forward);
+  }
+  delay(2500);
+  forward = !forward;
+}
+
+// ---------------------
+// Setup & Loop
+// ---------------------
 
 void setup()
 {
@@ -84,13 +121,5 @@ void setup()
 
 void loop()
 {
-  if (Serial.available() > 0) {
-
-    byte data[4]; // FL, FR, BL, BR
-    Serial.readBytes(data, 4);
-
-    for (int i=0; i<4; i++) {
-      wheelAr[i].drive(data[i]);
-    }
-  }
+  control_process();
 }
