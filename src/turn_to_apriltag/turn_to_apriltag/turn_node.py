807bb5caae266224ad450d7cd280879cd3063bc7
Cameron Spears <cameronjspears@icloud.com>
2025-10-19 14:45:40 -0400

    Edited turn_node.py to fix issues with variables names: specifically ac which was changed to april_tag_center. Due to updated interface, all width and height information was deleted and thus subscription to Image module was unnecessary and deleted. Also added significant numbers of preliminary and basic comments to begin documentation.

--- src/turn_to_apriltag/turn_to_apriltag/turn_node.py
+++ src/turn_to_apriltag/turn_to_apriltag/turn_node.py
@@ -2,47 +2,56 @@ import rclpy
 from rclpy.node import Node
 from apriltag_msgs.msg import AprilTagDetections
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import Image
 from turn_to_apriltag.PID import PID
 
-CONSTANT_p = 1
-CONSTANT_i = 1
-CONSTANT_d = 1
+##PID controller tunable variables
+CONSTANT_P = 1
+CONSTANT_I = 1
+CONSTANT_D = 1
+
+##Acceptable range of error from the center in pixels
+UPPER_X_ERROR = 0.1
+LOWER_X_ERROR = -0.1
 
 class TurnToAprilTagNode (Node):
     def __init__ (self):
         super().__init__('turn_to_apriltag')
+        ##where I publish
         self.sub = self.create_subscription(AprilTagDetections, 'awareness/apriltags', self.detection_callback, 10)
-        self.image_sub = self.create_subscription(Image, 'awareness/image_raw', self.image_callback, 10)
         self.pub = self.create_publisher(Twist, 'control/twist', 10)
-        self.controller = PID(CONSTANT_p, CONSTANT_i, CONSTANT_d)
-        self.width = 640
-        self.height = 480
-        
-
-    def image_callback(self, msg : Image):
-        self.width = msg.width
-        self.height = msg.height
+        ##intializes PID controller with the tunable variables
+        self.controller = PID(CONSTANT_P, CONSTANT_I, CONSTANT_D)
 
     def detection_callback(self, msg):
+        ##Robot controller
         twist = Twist()
+        ##Leave if there is no detection
         if not msg.detections:
             return 
+        ##if there is a tag
         for tag in msg.detections:
             if tag.tag_id == 0:
+                ##stops all movement
                 twist.linear.x = 0.0
-                sw = self.width
-                center_sx = sw//2
-                ac = tag.center
-                offset_x = ac[0] - center_sx
-                time = msg.header.stamp.sec + msg.header.stamp.nanosec/1e9
-                self.controller.set_goal(center_sx)
+
+                ##aprilTag Center
+                april_tag_center = tag.center
                 
+                ##difference between center of tag and the center of the camera
+                offset_x = (april_tag_center[0])
+                ##give the current time
+                time = msg.header.stamp.sec + msg.header.stamp.nanosec/1e9
+
+                ##set goal to be 0 which is the center of the image
+                self.controller.set_goal(0)
+                
+                ##calculates PID controller values
                 controller_value = self.controller.get_value(time, offset_x)
-                if (offset_x < -5):
+
+                if (offset_x < LOWER_X_ERROR):
                     print(f"(left) {offset_x}")
                     twist.angular.z = controller_value
-                elif (offset_x > 5):
+                elif (offset_x > UPPER_X_ERROR):
                     print(f"(right) {offset_x}")
                     twist.angular.z = controller_value
                 else:
