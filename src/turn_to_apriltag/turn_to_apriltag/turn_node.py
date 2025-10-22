b73add732cf32c531158b298d72f8fbb9385fdff
Ivan Post <ivan.post24@gmail.com>
2025-10-22 20:18:36 +0000

    Move set_goal to correct place and properly lock PID controller accesses

--- src/turn_to_apriltag/turn_to_apriltag/turn_node.py
+++ src/turn_to_apriltag/turn_to_apriltag/turn_node.py
@@ -35,7 +35,7 @@ class TurnToAprilTagNode (Node):
 
         #subscribed to detection_callbacks
         self.sub = self.create_subscription(AprilTagDetections, 'awareness/apriltags', self.detection_callback, 10)
-        
+
         #publish to twist
         self.pub = self.create_publisher(Twist, 'control/twist', 10)
 
@@ -43,7 +43,10 @@ class TurnToAprilTagNode (Node):
         self.parameter_p = self.get_parameter('parameter_p').get_parameter_value().double_value
         self.parameter_i = self.get_parameter('parameter_i').get_parameter_value().double_value
         self.parameter_d = self.get_parameter('parameter_d').get_parameter_value().double_value
+
         self.controller = PID(self.parameter_p, self.parameter_i, self.parameter_d)
+        #set goal to be 0 which is the center of the image
+        self.controller.set_goal(0)
 
     def detection_callback(self, msg: AprilTagDetections):
         #Robot motor controller which this will publish to (update)
@@ -51,8 +54,8 @@ class TurnToAprilTagNode (Node):
 
         #Leave if there is no detection
         if not msg.detections:
-            return 
-        
+            return
+
         #pulls all tags from msg
         for tag in msg.detections:
             #selects only tag id 0
@@ -65,9 +68,9 @@ class TurnToAprilTagNode (Node):
         self.pub.publish(twist)
         return
 
-    #code to update parameters at runtime         
+    #code to update parameters at runtime
     def __parameters_update_callback(self, parameters: list[rclpy.Parameter]) -> None:
-        
+
         for param in parameters:
             if param.name == 'parameter_p':
                 with self.__pid_lock:
@@ -81,7 +84,6 @@ class TurnToAprilTagNode (Node):
                 with self.__pid_lock:
                     self.parameter_d
                     self.controller.reset()
-        
 
     def decide_linear_movement(self, tag, twist):
         #Shoe lace code copied from stack overflow https://stackoverflow.com/questions/41077185/fastest-way-to-shoelace-formula
@@ -98,7 +100,7 @@ class TurnToAprilTagNode (Node):
         #end of copied code
 
         #Adjust these paramters as needed for each camera
-        ideal_size = 0.25   
+        ideal_size = 0.25
         tolerance = 0.1
         if(size < ideal_size - tolerance):
             twist.linear.x = 0.1
@@ -110,25 +112,23 @@ class TurnToAprilTagNode (Node):
             twist.linear.x = 0.0
             print("Staying stationary")
         return
-    
+
     def set_twist_turn(self, tag, twist:Twist, input_time):
         #stops all movement
         twist.linear.x = 0.0
 
         #apriltag center
         april_tag_center = tag.center
-        
+
         #difference between center of tag and the center of the camera
         offset_x = (april_tag_center[0])
 
         #give the current time
         time = input_time
 
-        #set goal to be 0 which is the center of the image
-        self.controller.set_goal(0)
-        
-        #calculates PID controller values
-        controller_value = self.controller.get_value(time, offset_x)
+        with self.__pid_lock:
+            #calculates PID controller values
+            controller_value = self.controller.get_value(time, offset_x)
 
         #calculates and decides the way to turn
         if (offset_x < LOWER_X_ERROR):
@@ -138,10 +138,10 @@ class TurnToAprilTagNode (Node):
             print(f"(right) {offset_x}")
             twist.angular.z = controller_value
         else:
-            print(f"(center) {offset_x}")   
-            twist.angular.z = 0.0  
-            
-                
+            print(f"(center) {offset_x}")
+            twist.angular.z = 0.0
+
+
 
 def main():
     rclpy.init()
